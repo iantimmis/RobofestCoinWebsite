ed9ba67b42f2bbda46ed40477efef51f0af956be
IanTimmis <itimmis@ltu.edu>
2018-04-28 00:42:05 -0400

    Updated form

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+
+router.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname + '/../public/html/form.html'));
 });
+
 
 module.exports = router;
